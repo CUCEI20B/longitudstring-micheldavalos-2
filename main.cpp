c6441eb6ebed0be3181a262cb5f8554cc95b7bcc
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-09-29 09:55:50 -0500

    prueba01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cadena;
+
+    // cin >> cadena; // lee hasta espacio o \n
+    getline(cin, cadena); // lee hasta \n
+    cout << cadena.size() << endl;
 
     return 0;
 }
